85175cb0532b7b973487deaba58ed426a8d4fefd
siid0 <138368995+siid0@users.noreply.github.com>
2023-08-12 10:01:22 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -3,15 +3,12 @@ from telethon.sessions import StringSession
 import os
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
-
-session1 = os.environ.get("TERMUX")
-SESSION1 = os.environ.get("TERMUX")
-DEVLOO = os.environ.get("DEVLO")
-CHNA = os.environ.get("CHNA")
-sython1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
-
-
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+session = os.environ.get("TERMUX")
+SESSION = os.environ.get("TERMUX")
+token = os.environ.get("TOKEN")
+fifthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
 ispay2 = ['yes']
-
-
+bot.start()
